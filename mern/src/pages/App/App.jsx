50eb3e01f5fc637028f4611d38be3b93e333cb33
kennethchang <kenneth.chang@generalassemb.ly>
2022-09-29 11:38:00 -0700

    Implemented routes

--- mern/src/pages/App/App.jsx
+++ mern/src/pages/App/App.jsx
@@ -1,12 +1,22 @@
 import './App.css';
 import { useState } from 'react';
-// import 
+// Import the following components
+import AuthPage from '../AuthPage/AuthPage';
+import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState({})
   return (
     < main className="App" >
-      {user ? <NewOrderPage /> : <AuthPage />}
+      {user ?
+        <Routes>
+          <Route path="/orders/new" element={<NewOrderPage />} />
+        </Routes>
+        :
+        <AuthPage />}
     </main >
   );
 }
