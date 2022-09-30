15572a8bb1f5d0ef158f4d2a8ceb058a6e8f3ca9
Suresh Melvin Sigera <sureshsigera@gmail.com>
2022-09-30 17:38:39 -0400

    Create the JWT

--- mern/controllers/api/users.js
+++ mern/controllers/api/users.js
@@ -1,13 +1,24 @@
 const User = require('../../models/user');
+const jwt = require('jsonwebtoken');
 
 async function create(req, res) {
     try {
         const user = await User.create(req.body);
-        return res.json(user);
+        const token = createJWT(user);
+        return res.json(token);
     } catch (error) {
         res.status(400).json(error);
     }
 }
+
+function createJWT(user) {
+    return jwt.sign(
+        {user},
+        process.env.SECRET,
+        {expiresIn: '24h'}
+    );
+}
+
 
 module.exports = {
     create
