ff130d6cdb9730f5a8b3ac967c2e79c70d39237a
Suresh Melvin Sigera <sureshsigera@gmail.com>
2022-10-03 18:40:08 -0400

    Add the checkToken API Function and Call It

--- mern/src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ mern/src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -2,6 +2,7 @@ import {checkToken} from "../../utilities/users-service";
 
 export default function OrderHistoryPage() {
     async function handleCheckToken() {
+        // calling checkToken from service layer
         const expDate = await checkToken();
         console.log(`expDate is ${expDate}`);
     }
