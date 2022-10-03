13eace938c8292d2bfbba50dfc9f8569f1bc96a3
Suresh Melvin Sigera <sureshsigera@gmail.com>
2022-10-03 15:10:48 -0400

    login

--- mern/controllers/api/users.js
+++ mern/controllers/api/users.js
@@ -1,5 +1,6 @@
 const User = require('../../models/user');
 const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
 
 async function create(req, res) {
     try {
@@ -19,7 +20,20 @@ function createJWT(user) {
     );
 }
 
+async function login(req, res) {
+    try {
+        const user = await User.findOne({email: req.body.email});
+        if (!user) throw new Error();
+        const match = await bcrypt.compare(req.body.password, user.password);
+        if (!match) throw new Error();
+        const token = createJWT(user);
+        res.json(token);
+    } catch (err) {
+        res.status(400).json('Bad Credentials');
+    }
+}
 
 module.exports = {
-    create
-}
+    create,
+    login
+};
--- mern/routes/api/users.js
+++ mern/routes/api/users.js
@@ -4,5 +4,6 @@ const usersCtrl = require('../../controllers/api/users');
 
 // POST : http://localhost:3001/api/users
 router.post('/', usersCtrl.create);
+router.post('/login', usersCtrl.login);
 
 module.exports = router;
