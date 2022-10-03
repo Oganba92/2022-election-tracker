1db8f251ef68e01276f0dd71e275edd92e2d2d5e
Suresh Melvin Sigera <sureshsigera@gmail.com>
2022-10-03 13:25:15 -0400

    Setting the user State When the Page is Loaded or Refreshed

--- mern/src/utilities/users-service.js
+++ mern/src/utilities/users-service.js
@@ -10,3 +10,41 @@ export async function signUp(userData) {
         throw new Error("Invalid Sign Up");
     }
 }
+
+export function getToken() {
+    // read the token from the local storage
+    const token = localStorage.getItem('token');
+    // we cannot find a token
+    if (!token) return null;
+    // there is a token
+    // grab the payload from the JWT
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    if (payload.exp < Date.now() / 1000) {
+        // is expired
+        localStorage.removeItem('token');
+        return null;
+    }
+    // everything went well, return the token
+    return token;
+}
+
+export function getUser() {
+    // get the token
+    const token = getToken();
+    // return user
+    /*
+    {
+      "user": {
+            "name": "testuser",
+            "email": "testuser@abc.com",
+            "_id": "633b17c566e0c1b670182013",
+            "createdAt": "2022-10-03T17:11:33.376Z",
+            "updatedAt": "2022-10-03T17:11:33.376Z",
+            "__v": 0
+        },
+          "iat": 1664817093,
+          "exp": 1664903493
+        }
+     */
+    return token ? JSON.parse(atob(token.split('.')[1])).user : null;
+}
