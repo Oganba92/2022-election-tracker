f4ee120fb954a4f6a180905a79ff1ba19d035138
Suresh Melvin Sigera <sureshsigera@gmail.com>
2022-10-03 13:36:46 -0400

    Render the User's Name in <NavBar>

--- mern/src/pages/App/App.jsx
+++ mern/src/pages/App/App.jsx
@@ -6,15 +6,17 @@ import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import {Routes, Route} from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar';
+import {getUser} from '../../utilities/users-service';
 
 function App() {
-    const [user, setUser] = useState(null);
+    // set the user by calling getUser function
+    const [user, setUser] = useState(getUser());
 
     return (
         < main className="App">
             {user ?
                 <>
-                    <NavBar/>
+                    <NavBar user={user}/>
                     <Routes>
                         <Route path="/orders/new" element={<NewOrderPage/>}/>
                         <Route path="/orders" element={<OrderHistoryPage/>}/>
