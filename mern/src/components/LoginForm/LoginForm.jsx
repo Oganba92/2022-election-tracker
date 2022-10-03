2c174a4838154ea98a74e9b68e2dfb2f7a00a742
Suresh Melvin Sigera <sureshsigera@gmail.com>
2022-10-03 16:07:51 -0400

    add more comments

--- mern/src/components/LoginForm/LoginForm.jsx
+++ mern/src/components/LoginForm/LoginForm.jsx
@@ -2,17 +2,21 @@ import {useState} from 'react';
 import * as usersService from '../../utilities/users-service';
 
 export default function LoginForm({setUser}) {
+    // initial state, username and password is set to empty string
     const [credentials, setCredentials] = useState({
         email: '',
         password: ''
     });
+    // error code is set to empty string
     const [error, setError] = useState('');
 
+    // on change grab the data and update the state
     function handleChange(evt) {
         setCredentials({...credentials, [evt.target.name]: evt.target.value});
         setError('');
     }
 
+    // on submit call the API endpoint and setUser state
     async function handleSubmit(evt) {
         // Prevent form from being submitted to the server
         evt.preventDefault();
