2a6edbc811b4ef987d184b1aff864c0a904a704f
Suresh Melvin Sigera <sureshsigera@gmail.com>
2022-10-03 19:17:37 -0400

    issue with token exp

--- mern/routes/api/users.js
+++ mern/routes/api/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
 
+// GET /api/users/check-token
+router.get('/check-token', usersCtrl.checkToken);
+
 // POST : http://localhost:3001/api/users
 router.post('/', usersCtrl.create);
 
